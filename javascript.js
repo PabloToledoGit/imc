35f97739c67a36d4e15ff4d566cba43984420f90
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-08-07 14:44:40 -0300

    criando codigo

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+const botao = document.querySelector('#botaoimc');
+botao.addEventListener('click', () => {
+    let pesoinput = document.querySelector('#peso').value;
+    let alturainput = document.querySelector('#altura').value;
+
+    let peso = parseInt(pesoinput);
+    let altura = parseInt(alturainput);
+
+    let calculo = peso / (altura * altura);
+
+    if(calculo <= '20'){
+        document.querySelector('#resultado').innerHTML = 'estás no peso ideal';
+    }else{
+        document.querySelector('#resultado').innerHTML = ''
+    }
+});
 
 
 
